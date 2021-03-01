b69e01e11df4a84491eb05709a0cc5ad4ddca9d5
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-03-01 15:09:06 +0000

    updated comments table

--- src/database/comments.js
+++ src/database/comments.js
@@ -13,7 +13,6 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
-
       imgurl: {
         type: DataTypes.STRING(500),
         allowNull: true,
