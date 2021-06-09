c8d5e926fd3bfb6246b408b1c9133cd230dd8bd9
Mirela Ekic <72197783+mirelaekic@users.noreply.github.com>
2021-06-09 13:18:52 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,10 +7,18 @@ const servicesRouter = require("./routes");
 dotenv.config();
 // const pass = require("./passport");
 const database = require("./database");
+const http = require("http");
 
 const port = process.env.PORT || 9001;
 
 const server = express();
+
+//socket
+const httpServer = http.createServer(server);
+const io = require("socket.io")(httpServer);
+module.exports = io;
+const SocketManager = require("./socket.js");
+io.on("connection", SocketManager);
 server.set("trust proxy",1)
 server.enable("trust proxy")
 server.use(express.json());
@@ -26,8 +34,10 @@ server.use(cors({
 server.use(cookieParser());
 
 server.use("/insta", servicesRouter);
+console.log(database,"the data")
 database.sequelize.sync({ force: false }).then(() => {
-  server.listen(port, () => {
+  httpServer.listen(port, () => {
     console.log("running on port" + port);
   });
 });
+
